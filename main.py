23f894c877611fae6aa2f9b16ed437a68d03e3eb
koneika <96289279+koneika@users.noreply.github.com>
2024-11-04 00:16:22 +0200

    version - 0.0.2

--- main.py
+++ main.py
@@ -1,179 +1,54 @@
-# version 0.0.1
+# version 0.0.2
 import numpy
 import time
 import math
 import pygame
 import os
 
-# Looks like something custome.
-# Sure, because in python we
-# don't have a lot of perfect
-# things which from c++ or java.
-# So, that's why i use these
-# functions, because i want
-# feel free, when i write a
-# code
-
-# its easy to use this functions
-# here is two dimentional array, like in c++
 def createIntMatrix(rows, cols, value=0):
     return numpy.full((rows, cols), value, dtype=int)
 
-# for fps counting
-real_time = int(time.time())
-real_time_float = time.time()
-counter = 0
-counterForAverage = 0
-counterForAverage2 = 0
-
-def fpsCounter(x, y):
-    global real_time
-    global real_time_float
-    global counter
-    global counterForAverage
-    global counterForAverage2
-
-    # # # print(real_time_float)
-    # # if real_time == int(time.time()):
-    # counter += 1
-    #     # for i in range():
-    # # else:
-    
-    # counterForAverage += 1
-    # counterForAverage2 += 1
-    # counter = 0
-    # # real_time += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    # os.system("cls")
-    # os.system("clear")
-
-    # counter += 1
-
-    # counter -= 10
-    # if counter == 10:
-    #     print("fps: ", counter)
-    #     return counter
-    
-
-    # print(real_time, "==", int(time.time()))
-
-    # if real_time == int(time.time()):
-    #     print("hi every 1 sec")
-    #     real_time += 1
-
-    #     if real_time == ((int(time.time())) + 10):
-    #         real_time -= 10
-
-    # counter += 10
-    # if counter == 10:
-    #     print("fps: ", counter)
-    #     counter = 0
-    # for i in range():
-    # else:
-        # for beautiful show my fps counter and etc.
-        # os.system("cls")
-        # os.system("clear")
-
-    
-    # counterForAverage += 1
-    # counterForAverage2 += counter
-    
-    # real_time += 1
-    # print("average fps: ", counterForAverage2/counterForAverage)
-    print("coordinates: ", x, y)
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # else:
-        # for beautiful show my fps counter and etc.
-        # os.system("cls")
-        # os.system("clear")
-    
-    # if real_time == int(time.time()):
-    #     print("fps: ", counter)
-    # else:
-    #     print("fps: ", counter)
-    
-    # print("coordinates: ", x, y)
-    # os.system("cls")
-    # os.system("clear")
-    
-        
-    # print("average fps: ", counterForAverage2/counterForAverage)
-    
-    
 def coordinateCounter(x, y):
-    # global real_time
+    os.system("clear")
+    return "coordinates: ", x, y
 
-    # print(real_time)
+def fpsLimit(fpsForLimit: float = 0):
 
-    # await fpsCounter(x, y)
-    # time.sleep(0.1)
-    # print("coordinates: ", x, y)
-    # if real_time == int(time.time()):
-    #     print("fps: ", counter)
-    #     real_time += 1
-    # if real_time == int(time.time()):
-    #     print("hi")
-    time.sleep(0.1)
+    if fpsForLimit == 0:
+        # counter += 1
+        # if (1*fpsLimit) <= counter:
+            
+        #     counter -= fpsLimit 
+        pass
+    elif(fpsForLimit != 0):
+        time.sleep(1/fpsForLimit)
+        os.system("clear")
+        return "ms: ", 1/fpsForLimit
+    #elif()
+    # fps counter
+    # not work, because time in python = don't care
+    # try to figur out about "time.perf_counter" the next time
+    # for solving that problem
+    # return true ms from fps counter here
+    # but im the next time or good mood
 
+    # also i wanted to do optimized counter
+    # for example
+    # update my x and y 10 times per second
+    # but fps
+    # update it every 1 time per second
+
+def debug(x, y):
+    print(fpsLimit(60), coordinateCounter(x, y))
 
 def main():
-    global real_time
-    global real_time_float
-    global counter
-    global counterForAverage
-    global counterForAverage2
     
     width, height = 800, 600
     black = (0, 0, 0)
     white = (255, 255, 255)
     x, y = 0, 0
+    counter = 0
+    # fpsLimit = 10
 
     pygame.init()
     screen = pygame.display.set_mode((width, height))
@@ -181,25 +56,18 @@ def main():
     
 
     while True:
-        time.sleep(0.1)
-        
         pygame.event.pump()
+
         x, y = pygame.mouse.get_pos()
-        # map = createIntMatrix(100, 100, 1)
-        # width += 1
+        debug(x, y)
 
         screen.fill(black)
 
         for i in range(int(math.pow(2, 10))):
             pygame.draw.line(screen, white, (0,0), (width//2, height//2))
+
         
-        #await coordinateCounter(x, y)
-        print(fpsCounter(x, y))
-        # counter += 1
-        # print(counter)
         pygame.display.flip()
-        
-        # clock.tick(60)
     
         
 
